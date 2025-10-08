f5b8fcee7547ad1833deea802a1dd2a5695c4c10
Felix0911 <39178539+Felix0911@users.noreply.github.com>
2025-10-08 12:11:30 -0400

    Refactor checkpoint loading to display model state keys and tensor shapes

--- scripts/run_tests_fei/check_checkpoint.py
+++ scripts/run_tests_fei/check_checkpoint.py
@@ -3,6 +3,13 @@ checkpoint_path = "/home/fzhcis/data/semantic3d_full/selected/checkpoints/ckpt_0
 ckpt = torch.load(checkpoint_path, map_location="cpu")
 
 # List the top-level keys
-print(ckpt.keys())
-# print(ckpt['config'])
-print(ckpt['state_dict'].keys())
+state_dict = ckpt["model_state_dict"]
+
+print(list(state_dict.keys())[:20])  # show first 20 keys
+
+# Find the first and last weight tensors
+first_key = list(state_dict.keys())[0]
+last_key = list(state_dict.keys())[-1]
+
+print(first_key, state_dict[first_key].shape)
+print(last_key, state_dict[last_key].shape)
