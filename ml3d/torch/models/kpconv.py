d11e9dfed7fc8699bacabb4fa33d29ba061c926b
Fei Zhang <fzhcis@rit.edu>
2025-10-30 23:10:30 -0400

    Refactor KDTree handling in KPFCNN to avoid pickling issues with multiprocessing

--- ml3d/torch/models/kpconv.py
+++ ml3d/torch/models/kpconv.py
@@ -380,15 +380,12 @@ class KPFCNN(BaseModel):
                 labels=labels,
                 grid_size=cfg.first_subsampling_dl)
 
-        # Note: KDTree cannot be pickled for multiprocessing (num_workers > 0)
-        # We build it temporarily here only for projection indices, then rebuild in transform()
         search_tree = KDTree(sub_points)
 
         data['point'] = sub_points
         data['feat'] = sub_feat
         data['label'] = sub_labels
-        # Don't store search_tree - it cannot be pickled for multiprocessing!
-        # It will be rebuilt in transform() from points
+        data['search_tree'] = search_tree
 
         if split in ["test", "testing", "validation", "valid"]:
             proj_inds = np.squeeze(
@@ -403,10 +400,7 @@ class KPFCNN(BaseModel):
         points = data['point']
         sem_labels = data['label']
         feat = data['feat']
-        
-        # Rebuild KDTree from points (not stored in data to avoid pickling issues)
-        # This allows num_workers > 0 in DataLoader
-        search_tree = KDTree(points)
+        search_tree = data['search_tree']
 
         dim_points = points.shape[1]
         if feat is None:
@@ -544,8 +538,7 @@ class KPFCNN(BaseModel):
         self.inference_ori_data = data
         self.inference_data = self.preprocess(data, attr)
         self.inference_proj_inds = self.inference_data['proj_inds']
-        # Get number of points from the preprocessed point data instead of search_tree
-        num_points = self.inference_data['point'].shape[0]
+        num_points = self.inference_data['search_tree'].data.shape[0]
 
         self.possibility = np.random.rand(num_points) * 1e-3
         self.test_probs = np.zeros(shape=[num_points, self.cfg.num_classes],
